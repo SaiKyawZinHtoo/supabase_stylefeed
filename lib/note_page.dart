d93494a6a92d77a72ec32aa7920a06b5186f19bc
SaiKyawZinHtoo <saikyawzinhtoo381@gmail.com>
2024-12-22 15:27:53 +0630

    Implement note management features: add, update, and delete notes with UI updates

--- lib/note_page.dart
+++ lib/note_page.dart
@@ -16,6 +16,12 @@ class _NotePageState extends State<NotePage> {
   //text controller
   final textController = TextEditingController();
 
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
   // user wants to create a new note
   void addNewNote() {
     showDialog(
@@ -36,15 +42,16 @@ class _NotePageState extends State<NotePage> {
           ),
           //save button
           TextButton(
-            onPressed: () {
+            onPressed: () async {
               //create a new note
               final newNote = Note(
                 content: textController.text,
               );
               //save in db
-              notesDatabase.createNote(newNote);
+              await notesDatabase.createNote(newNote);
               Navigator.pop(context);
               textController.clear();
+              setState(() {}); // Update the UI
             },
             child: const Text("Save"),
           )
@@ -54,8 +61,68 @@ class _NotePageState extends State<NotePage> {
   }
 
   // user wants to update a note
+  void updateNote(Note note) {
+    textController.text = note.content;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Updated Notes"),
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          //cancel button
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text("Cancel"),
+          ),
+          //save button
+          TextButton(
+            onPressed: () async {
+              await notesDatabase.updateNote(note, textController.text);
+              Navigator.pop(context);
+              textController.clear();
+              setState(() {}); // Update the UI
+            },
+            child: const Text("Save"),
+          )
+        ],
+      ),
+    );
+  }
 
   // user wants to delete a note
+  void deleteNote(Note note) async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Delete Notes"),
+        actions: [
+          //cancel button
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text("Cancel"),
+          ),
+          //save button
+          TextButton(
+            onPressed: () async {
+              await notesDatabase.delete(note);
+              Navigator.pop(context);
+              textController.clear();
+              setState(() {}); // Update the UI
+            },
+            child: const Text("Save"),
+          )
+        ],
+      ),
+    );
+  }
 
   //Build the UI
   @override
@@ -65,6 +132,44 @@ class _NotePageState extends State<NotePage> {
       appBar: AppBar(
         title: Text("Notes"),
       ),
+      //body -> StreamBuilder
+      body: StreamBuilder<List<Note>>(
+          stream: notesDatabase.stream,
+          builder: (context, snapshot) {
+            //loading..
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            //loading!
+            final notes = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                final note = notes[index];
+                return ListTile(
+                  title: Text(note.content),
+                  trailing: SizedBox(
+                    width: 100,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.edit),
+                          onPressed: () => updateNote(note),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () => deleteNote(note),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }),
       //button
       floatingActionButton: FloatingActionButton(
         onPressed: addNewNote,
